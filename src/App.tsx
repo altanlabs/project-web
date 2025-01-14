a49077447e684544447e74452d4a079f59759b35
Altan <albert@altan.ai>
2025-01-14 18:03:42 +0100

    Add navigation to header in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import IndexPage from './pages/index';
 import BlogPage from './pages/blog';
 import ProjectsPage from './pages/projects';
@@ -9,8 +9,16 @@ function App() {
     <Router>
       <div>
         <header className="bg-gray-800 text-white p-4">
-          <div className="container mx-auto">
+          <div className="container mx-auto flex justify-between items-center">
             <h1 className="text-2xl font-bold">Antonio's Personal Website</h1>
+            <nav>
+              <ul className="flex space-x-4">
+                <li><Link to="/" className="hover:underline">Home</Link></li>
+                <li><Link to="/blog" className="hover:underline">Blog</Link></li>
+                <li><Link to="/projects" className="hover:underline">Projects</Link></li>
+                <li><Link to="/cv" className="hover:underline">CV</Link></li>
+              </ul>
+            </nav>
           </div>
         </header>
         <main>
